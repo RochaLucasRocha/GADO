0c1941bb0fb6c308c4ba059004e7c0246071a84b
Rocha <76980451+RochaLucasRocha@users.noreply.github.com>
2022-02-17 14:31:55 -0300

    Js no html compra

---
+++ JavaScript/compra.js
@@ -0,0 +1,27 @@
+
+function cadastrar(){
+    var data = new Date()
+    var ano = data.getFullYear()
+    var hoje = document.getElementById('data')
+    var identidade = document.getElementById('identdd')
+    var idade = document.getElementById('idade')
+    var peso = document.getElementById('peso')
+    var preco = document.getElementById('preco')
+    var sex = document.getElementsByName('radsex')
+    var vend = document.getElementById('vend')
+
+    var res = document.querySelector('div.log')
+
+    if(hoje.value.length == 0 || Number(hoje.value) > ano){
+        window.alert('[ERRO] verifique a Data')
+    }
+    var genero = ''
+    if (sex[0].checked){
+        genero = 'Macho'
+    }else if (sex[1].checked){
+        genero = 'Femea'
+    }
+    res.style.marginTop = '30px'
+    res.style.textAlign = 'center'
+    res.innerHTML = `Data : ${hoje.value} <br>Identificação: ${identidade.value} <br>Idade :${idade.value} <br>Peso :${peso.value} <br>Preço :${preco.value} <br>Sexo :${genero} <br>Vendedor :${vend.value}`
+}
